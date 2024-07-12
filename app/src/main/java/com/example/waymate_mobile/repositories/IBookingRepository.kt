c1b94f7480ec15f837e9e988b5af139381112b72
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-12 19:20:50 +0200

    Create IBookingRepository.kt

---
+++ app/src/main/java/com/example/waymate_mobile/repositories/IBookingRepository.kt
@@ -0,0 +1,9 @@
+package com.example.waymate_mobile.repositories
+
+import com.example.waymate_mobile.dtos.booking.DtoInputBooking
+import retrofit2.http.GET
+
+interface IBookingRepository {
+    @GET("booking")
+    suspend fun getAllTrip(): List<DtoInputBooking>
+}
