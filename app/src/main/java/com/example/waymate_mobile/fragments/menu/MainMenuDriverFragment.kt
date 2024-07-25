fdea079f685e80451cbc49b7975e1bb8b3e2bed3
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-25 02:43:40 +0200

    Create main menu driver fragment

---
+++ app/src/main/java/com/example/waymate_mobile/fragments/menu/MainMenuDriverFragment.kt
@@ -0,0 +1,36 @@
+package com.example.waymate_mobile.fragments.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.waymate_mobile.activities.MainActivity
+import com.example.waymate_mobile.databinding.FragmentMainMenuDriverBinding
+
+class MainMenuDriverFragment : Fragment() {
+    private lateinit var binding: FragmentMainMenuDriverBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainMenuDriverBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDisconnection.setOnClickListener {
+            (requireActivity() as MainActivity).signOut()
+        }
+
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainMenuDriverFragment()
+    }
+}
