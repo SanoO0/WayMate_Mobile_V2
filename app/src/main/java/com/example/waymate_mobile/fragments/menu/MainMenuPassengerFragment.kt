b46876518a11243f9dea2fbf911187252d790652
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-25 14:15:13 +0200

    Create main menu passenger fragment with binding and sign out method

---
+++ app/src/main/java/com/example/waymate_mobile/fragments/menu/MainMenuPassengerFragment.kt
@@ -0,0 +1,35 @@
+package com.example.waymate_mobile.fragments.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.waymate_mobile.R
+import com.example.waymate_mobile.activities.MainActivity
+import com.example.waymate_mobile.databinding.FragmentMainMenuPassengerBinding
+
+class MainMenuPassengerFragment : Fragment() {
+    private lateinit var binding: FragmentMainMenuPassengerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainMenuPassengerBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDisconnection.setOnClickListener {
+            (requireActivity() as MainActivity).signOut()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainMenuPassengerFragment()
+    }
+}
