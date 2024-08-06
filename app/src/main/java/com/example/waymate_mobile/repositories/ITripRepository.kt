dbb01168daa8c615330012f03683f71e7b158cf9
SanoO0 <mathiasmanzano37@gmail.com>
2024-08-06 22:45:43 +0200

    Add api method to get trips with filter

--- app/src/main/java/com/example/waymate_mobile/repositories/ITripRepository.kt
+++ app/src/main/java/com/example/waymate_mobile/repositories/ITripRepository.kt
@@ -2,8 +2,9 @@ package com.example.waymate_mobile.repositories
 
 import com.example.waymate_mobile.dtos.trip.DtoInputTrip
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ITripRepository {
-    @GET("trip")
-    suspend fun getAllTrip(): List<DtoInputTrip>
+    @GET("/api/v1/trip/tripByFilter")
+    suspend fun getAllTrip(@Query("userCount") count: Int): List<DtoInputTrip>
 }
