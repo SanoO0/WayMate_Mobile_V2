93deb6a49b08151569435a99cecf683c90193676
SanoO0 <mathiasmanzano37@gmail.com>
2024-08-13 01:50:02 +0200

    Comment UserTypeService

--- app/src/main/java/com/example/waymate_mobile/services/UserTypeService.kt
+++ app/src/main/java/com/example/waymate_mobile/services/UserTypeService.kt
@@ -15,27 +15,39 @@ import kotlin.coroutines.suspendCoroutine
 class UserTypeService(private val context: Context) {
     private lateinit var authenticationRepository: IAuthenticationRepository
     lateinit var prefs: SharedPreferences
-
+    // Suspend function to retrieve the user type from the server
     suspend fun getUserType(): String = suspendCoroutine { continuation ->
+        // Initialize shared preferences
         prefs = context.getSharedPreferences("waymate", Context.MODE_PRIVATE)
         val jwtToken = prefs.getString("jwtToken", "") ?: ""
-        authenticationRepository = RetrofitFactory.create(jwtToken, IAuthenticationRepository::class.java)
+        // Initialize the authentication repository with the JWT token
+        authenticationRepository =
+            RetrofitFactory.create(jwtToken, IAuthenticationRepository::class.java)
+        // Make a network call to get the user type
         val call = authenticationRepository.getUserType()
         call.enqueue(object : Callback<DtoInputAuthentication> {
-            override fun onResponse(call: Call<DtoInputAuthentication>, response: Response<DtoInputAuthentication>) {
+            // Handle the response from the server
+            override fun onResponse(
+                call: Call<DtoInputAuthentication>,
+                response: Response<DtoInputAuthentication>
+            ) {
                 if (response.isSuccessful) {
+                    // Resume the coroutine with the user type if the response is successful
                     val userType = response.body()?.userType
                     userType?.let {
                         continuation.resume(userType)
                     }
                 } else {
-                    val message = "echec : ${response.message()}"
-                    Log.d("Echec", message)
+                    // Log the failure message if the response is not successful
+                    val message = "Failed : ${response.message()}"
+                    Log.d("Failure", message)
                 }
             }
-            override fun onFailure(call: Call<DtoInputAuthentication>, t:Throwable) {
-                val message = "Echec DB: ${t.message}"
-                Log.e("EchecDb", message, t)
+            // Handle failure of the network call
+            override fun onFailure(call: Call<DtoInputAuthentication>, t: Throwable) {
+                // Log the error message if the network request fails
+                val message = "Database Failure: ${t.message}"
+                Log.e("DbFailure", message, t)
             }
         })
     }
