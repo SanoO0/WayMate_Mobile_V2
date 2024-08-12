81d24e26d01d8cb32c165c553a648800fd5c6622
SanoO0 <mathiasmanzano37@gmail.com>
2024-08-13 01:57:53 +0200

    Comment RetrofitFactory and OkHttpClientFactory

--- app/src/main/java/com/example/waymate_mobile/utils/OkHttpClientFactory.kt
+++ app/src/main/java/com/example/waymate_mobile/utils/OkHttpClientFactory.kt
@@ -7,22 +7,26 @@ import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
 object OkHttpClientFactory {
+    // Function to create a customized OkHttpClient instance
     fun create(token: String): OkHttpClient {
+        // Custom CookieJar to handle cookies manually
         val cookieJar = object : CookieJar {
+            // List to store cookies
             private val cookies = mutableListOf<Cookie>()
-
+            // Save cookies received from the server
             override fun saveFromResponse(url: HttpUrl, cookies: MutableList<Cookie>) {
                 this.cookies.addAll(cookies)
             }
-
+            // Load cookies for each request
             override fun loadForRequest(url: HttpUrl): MutableList<Cookie> {
                 return cookies
             }
         }
-
+        // Create and configure OkHttpClient instance
         val okHttpClient = OkHttpClient.Builder()
             .callTimeout(30, TimeUnit.SECONDS)
             .addInterceptor { chain ->
+                // Interceptor to add the JWT token as a cookie in each request
                 val original = chain.request()
                 val requestBuilder = original.newBuilder()
                 val cookieHeader = "WaymateSession=$token"
