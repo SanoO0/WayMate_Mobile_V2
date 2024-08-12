b7978ccaf58828dcf77ecad30a34f211ae958051
SanoO0 <mathiasmanzano37@gmail.com>
2024-08-13 01:30:43 +0200

    Add comment to SignIn Activity

--- app/src/main/java/com/example/waymate_mobile/activities/SigninActivity.kt
+++ app/src/main/java/com/example/waymate_mobile/activities/SigninActivity.kt
@@ -23,13 +23,16 @@ class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Initialize view binding
         binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize shared preferences and repository
         prefs = getSharedPreferences("waymate", Context.MODE_PRIVATE)
         jwtToken = prefs.getString("jwtToken", "") ?: ""
         authenticationRepository = RetrofitFactory.create(jwtToken, IAuthenticationRepository::class.java)
 
+        // Set up sign-in button click listener
         binding.btnSignIn.setOnClickListener {
             submitForm(
                 binding.etLogin.text.toString(),
@@ -39,33 +42,40 @@ class SignInActivity : AppCompatActivity() {
         }
     }
 
+    // Submits the sign-in form by sending a request to the authentication repository
     private fun submitForm(email: String, password: String, stay: Boolean) {
         lifecycleScope.launch {
             try {
+                // Attempt to sign in with the provided email and password
                 authenticationRepository.signIn(DtoOutputSigIn(email, password, stay)).let { response ->
                     if (response.isSuccessful) {
+                        Log.d("Sign In", "Successful")
+                        // If sign-in is successful, extract and save the JWT token
                         response.headers().get("Set-Cookie")?.let { cookieHeader ->
                             extractTokenFromCookie(cookieHeader)?.let { token ->
                                 updateTokenInPreferences(token)
                             }
-
                         }
+                        // Navigate to the main activity
                         navigateToMainActivity()
-
                     } else {
-                        Log.d("ErrorAuth", "Error Auth: ${response.message()}")
+                        // Handle unsuccessful sign-in attempts
+                        Log.e("ErrorAuth", "Error Auth: ${response.message()}")
                         Toast.makeText(this@SignInActivity, "Email or Password incorrect", Toast.LENGTH_SHORT).show()
-                        binding.etLogin.text?.clear()
-                        binding.etPassword.text?.clear()
+                        clearFormFields()
                     }
                 }
             } catch (e: Exception) {
+                // Log any errors that occur during the sign-in process
                 Log.e("ErrorDb", "Error DB: ${e.message}", e)
             }
         }
     }
 
+    // Extracts the JWT token from the "Set-Cookie" header
     private fun extractTokenFromCookie(cookieHeader: String): String? = Pattern.compile("WaymateSession=([^;]+)").matcher(cookieHeader).takeIf { it.find() }?.group(1)
+
+    // Updates the JWT token in shared preferences
     private fun updateTokenInPreferences(newToken: String) {
         getSharedPreferences("waymate", MODE_PRIVATE).edit().apply {
             putString("jwtToken", newToken)
@@ -73,7 +83,14 @@ class SignInActivity : AppCompatActivity() {
         }
     }
 
+    // Navigates to the main activity
     private fun navigateToMainActivity() {
         startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+    }
+
+    // Clears the form fields (email and password)
+    private fun clearFormFields() {
+        binding.etLogin.text?.clear()
+        binding.etPassword.text?.clear()
     }
 }
